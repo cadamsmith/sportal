aac457bcaf5b0782a9c89e455057396580e3fbe4
CoryBrasher <65250652+CoryBrasher@users.noreply.github.com>
2022-02-13 11:06:23 -0600

    Last Bit

--- sportal/lib/screens/event_details_page.dart
+++ sportal/lib/screens/event_details_page.dart
@@ -1,20 +1,83 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class EventDetailsPage extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter App Learning',
+//       theme: ThemeData(
+//         primarySwatch: Colors.green,
+//       ),
+//       home: MyHomePage()
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key? key}) : super(key: key);
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// ignore: camel_case_types, must_be_immutable
+class eventDetails extends StatelessWidget {
   static const id = 'eventDetails';
   final String eventID;
   //CollectionReference data = FirebaseFirestore.instance.collection('Events');
-  EventDetailsPage({Key? key, required this.eventID}) : super(key: key);
+  eventDetails({required this.eventID});
 
-  //TODO: This would have to pull from firebase
+  var roster = [
+    'Tyler Hirn',
+    'Cory Brasher',
+    'Adam Smith',
+    'Reid Watson',
+    'Dwayne Johnson'
+  ];
+  getRoster() async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    await db.collection('Players').get().then((querySnapshot) {
+      // ignore: avoid_function_literals_in_foreach_calls
+      querySnapshot.docs.forEach((element) {
+        roster.add(element.data()["Name"]);
+      });
+    });
+  }
+
+  getTitle() {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    // ignore: non_constant_identifier_names
+    String Title = 'Bean Bag Toss IM Fields';
+
+    db.collection('Events').get().then((querySnapshot) {
+      // ignore: avoid_function_literals_in_foreach_calls
+      querySnapshot.docs.forEach((element) {
+        if (element.id == eventID) {
+          Title = element.data()["Name"].toString();
+          Title = Title + " " + element.data()["Location"];
+        }
+      });
+    });
+
+    return Text(Title);
+  }
+
   List<String> images = [
-    "assets/images/scenary.jpg",
-    "assets/images/scenary_red.jpg",
-    "assets/images/waterfall.jpg",
-    "assets/images/tree.jpg",
+    "images/PF1.jpg",
+    "images/PF2.jpeg",
+    "images/PF3.jpg",
+    "images/PF4.jpeg",
+    "images/PF5.jpeg",
   ];
   @override
   Widget build(BuildContext context) {
+    getRoster();
+
+    int counter = 0;
     return MaterialApp(
         title: 'Event Details',
         theme: ThemeData(
@@ -22,24 +85,25 @@ class EventDetailsPage extends StatelessWidget {
         ),
         home: Scaffold(
             appBar: AppBar(
-              title: const Text(
-                  "Basketball 4/12"), //TODO: Pull the title of the event here.
+              title: getTitle(),
             ),
             body: ListView.builder(
               // ignore: avoid_types_as_parameter_names, non_constant_identifier_names
               itemBuilder: (BuildContext, index) {
-                //TODO: load all the teams and items in the list from firebase
+                if(index%2==0){
+                  counter += 1;
+                }
                 return Card(
                   child: ListTile(
                     leading: CircleAvatar(
                       backgroundImage: AssetImage(images[index]),
                     ),
-                    title: Text("Brody Hall"),
-                    trailing: Text("Team #1"),
+                    title: Text(roster[index]),
+                    trailing: Text('Team #' + counter.toString()),
                   ),
                 );
               },
-              itemCount: images.length,
+              itemCount: roster.length,
               shrinkWrap: true,
               padding: const EdgeInsets.all(5),
               scrollDirection: Axis.vertical,
