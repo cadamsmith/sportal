61524dabe596ca7e5e3b6fab27dafd438bcdb164
cadamsmith <41703860+cadamsmith@users.noreply.github.com>
2022-02-13 10:38:19 -0600

    links now

--- sportal/lib/screens/map_page.dart
+++ sportal/lib/screens/map_page.dart
@@ -5,6 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sportal/brand_colors.dart';
+import 'package:sportal/screens/create_event_page.dart';
+
+import 'event_details_page.dart';
 
 class MapPage extends StatefulWidget {
   static const String id = 'map';
@@ -30,7 +34,7 @@ class _MapPageState extends State<MapPage> {
   );
 
   //list of markers
-  List<Marker> markers = [];
+  Set<Marker> markers = {};
 
   void setupPositionLocator() async {
     Position position = await Geolocator.getCurrentPosition(
@@ -54,15 +58,38 @@ class _MapPageState extends State<MapPage> {
       // ignore: avoid_function_literals_in_foreach_calls
       querySnapshot.docs.forEach((element) {
         //TODO Create a system to check for questions already asked.
-        
+
         var lat = element.data()["Latitude"];
         var long = element.data()["Longitude"];
-        
-        Marker marker = Marker(markerId: MarkerId(element.id), position: LatLng(lat,long));
+
+        Marker marker = Marker(
+            markerId: MarkerId(element.id),
+            position: LatLng(lat, long),
+            onTap: () {
+              goToEventDetailsPage(element.id);
+            });
 
         markers.add(marker);
       });
     });
+  }
+
+  void goToEventDetailsPage(String eventID) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EventDetailsPage(eventID: eventID),
+      ),
+    );
+  }
+
+  void goToCreateEvent() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const CreateEventPage(),
+      ),
+    );
   }
 
   @override
@@ -71,49 +98,41 @@ class _MapPageState extends State<MapPage> {
       drawer: const Drawer(
         child: Text('text'),
       ),
-      floatingActionButton: SizedBox(
-        height: 40,
-        width: 40,
-        child: Builder(
-          builder: (context) {
-            return FloatingActionButton(
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-              backgroundColor: Colors.white,
-              child: const Icon(
-                Icons.menu,
-                color: Colors.black87,
-              ),
-            );
-          },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          goToCreateEvent();
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.black87,
         ),
+        backgroundColor: BrandColors.primary,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndTop,
       body: Stack(
         children: [
-          builder: Builder(
-            return GoogleMap(
-      padding: EdgeInsets.only(
-        bottom: mapBottomPadding,
-      ),
-      mapType: MapType.normal,
-      myLocationButtonEnabled: true,
-      myLocationEnabled: true,
-      initialCameraPosition: _kGooglePlex,
-      markers: markers,
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-        mapController = controller;
+          Builder(
+            builder: (_) {
+              getMarkers();
 
-        setState(() {
-          mapBottomPadding = Platform.isAndroid ? 280 : 270;
-        });
+              return GoogleMap(
+                padding: EdgeInsets.only(
+                  bottom: mapBottomPadding,
+                ),
+                mapType: MapType.normal,
+                myLocationButtonEnabled: true,
+                myLocationEnabled: true,
+                initialCameraPosition: _kGooglePlex,
+                markers: markers,
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
+                  mapController = controller;
 
-        setupPositionLocator();
-      },
-    );
-          )
+                  setupPositionLocator();
+                },
+              );
+            },
+          ),
         ],
       ),
     );
